0393c85c9f950f9766368b979fbb1e62ff2fab6c
junhee <jhhan718@gmail.com>
2024-11-08 09:17:44 +0900

    springboot,maven프로젝트개설(header,footer,loginview구현)

---
+++ moimon/src/main/java/com/future/my/MoimonApplication.java
@@ -0,0 +1,13 @@
+package com.future.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoimonApplication.class, args);
+	}
+
+}
